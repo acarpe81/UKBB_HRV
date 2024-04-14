2ce233f1038750e2997d9b0c5a4696d45748956e
acarpe81 <carpenter.alexander@gmail.com>
2024-04-14 01:28:11 +0100

    Fix bug in login functionality

--- GRS.r
+++ GRS.r
@@ -1,7 +1,7 @@
 library(devtools)
 source_url("https://raw.githubusercontent.com/hdg204/Rdna-nexus/main/install.R")
 
-# Import the file noltegrstxt2.txt into the current directory
+# Import the GRS and HRV files into the current directory
     system2("dx", args = c("download", "/Alexander/noltegrstxt2.txt"))
     system2("dx", args = c("download", "/Alexander/hrv_11_04_24.csv"))
 
@@ -9,7 +9,7 @@ source_url("https://raw.githubusercontent.com/hdg204/Rdna-nexus/main/install.R")
     noltegrs_df <- read.table("noltegrstxt2.txt", header = TRUE)
     hrv_df <- read.csv("hrv_11_04_24.csv")
 
-# Generate GRS using eid column of hrv_df as input
+# Generate GRS using eid from hrv_df as input
     a <- generate_grs(hrv_df)
     #grs_list <- list(grs1 = a$grs, grs2 = a$grs)
     #hrv_df$grs1 <- a$grs
