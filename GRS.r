2eb60f503987c8b081c01d599df47f33021a4840
acarpe81 <carpenter.alexander@gmail.com>
2024-04-16 22:15:51 +0100

    Updated GRS code to handle build variation

--- GRS.r
+++ GRS.r
@@ -2,7 +2,7 @@ library(devtools)
 source_url("https://raw.githubusercontent.com/hdg204/Rdna-nexus/main/install.R")
 
 # Import the GRS and HRV files into the current directory
-    system2("dx", args = c("download", "/Alexander/noltegrstxt2.txt"))
+    system2("dx", args = c("download", "/Alexander/nolte_mfi.txt"))
     system2("dx", args = c("download", "/Alexander/hrv_11_04_24.csv"))
 
 # Read the HRV file as a dataframe
@@ -10,7 +10,8 @@ source_url("https://raw.githubusercontent.com/hdg204/Rdna-nexus/main/install.R")
     hrv_df <- read.csv("hrv_11_04_24.csv")
 
 # Generate GRS 
-    a <- generate_grs('noltegrstxt2.txt')
+    a <- generate_grs('nolte_mfi.txt')
 
+saveRDS(a, file = "GRS.rds")
+system('dx upload GRS.rds --path /Alexander/')
 
-# Need to convert the GRS SNPs from hg19 to hg38 using a liftover tool
